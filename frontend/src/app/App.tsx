30f36bccbf2bd81984b9131de624eaebb9d4f748
YoonGangHoon <yghun021007@naver.com>
2026-05-12 20:27:46 +0900

    Feat: 로그인 기능 삭제

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,6 @@
 import { RouterProvider } from 'react-router';
-import { AuthProvider } from './auth';
 import { router } from './routes';
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  );
+  return <RouterProvider router={router} />;
 }
